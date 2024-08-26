ce2f7418f2575a22719190ddba419739e25d9e50
Jonathan Alcântara <jonathan.infra@alterdata.com.br>
2024-08-26 15:14:39 -0300

    Trabalhando com DRY

--- setup/settings.py
+++ setup/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-#Indica onde esão todos os arquivos estáticos
+#Indica onde estão todos os arquivos estáticos
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'setup/static')
     
